9a2a09a4c6d2b545e3390c3945635f1368d04b39
pavanp0309 <pavanreddyp0309@gmail.com>
2025-03-25 13:14:29 +0530

    routing completed

--- lazyguy/src/pages/Home.jsx
+++ lazyguy/src/pages/Home.jsx
@@ -1,38 +1,82 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link,useLocation,useNavigate } from "react-router-dom";
 import { ProductData } from "../api/Data";
+import Products from "./Products";
 
 const Home = () => {
+  const location=useLocation() // its is used for destructuring the url
+  console.log(location)//{pathname: '/', search: '', hash: '', state: null, key: '5w1qe651'}
+const navigate=useNavigate() // programmatic Navigation 
+
+  let queryparams=new URLSearchParams(location.search)
+  console.log(queryparams)
+  let category=queryparams.get("category")
+  let sort=queryparams.get("sort")
+
+  let filterData=ProductData.filter((prod)=>category?prod?.category===category:true)
+  if(sort=="asc"){
+    filterData.sort((a,b)=>a.price-b.price)
+  }else if(sort=="des"){
+    filterData.sort((a,b)=>b.price-a.price)
+  }
+
+
+  // function for Handling the Fileters
+  function handleFilter(key,value){
+    if(value){
+      queryparams.set(key ,value)
+    }else{
+      queryparams.delete(key,value)
+    }
+    navigate(`?${queryparams.toString()}`)
+  }
+
+
+
   return (
-    <div>
+    <>
+    <div className="container my-3 d-flex justify-content-between">
       <ul className="nav nav-pills">
         <li className="nav-item">
-          <Link className="nav-link active" aria-current="page" to={''}>
-           ALL Products
-          </Link>
+         <button className="btn btn-outiline-success" onClick={()=>handleFilter()}>AllProducts</button>
         </li>
         <li className="nav-item">
-          <Link className="nav-link" to={''}>
-            Electronics
-          </Link>
+        <li className="nav-item">
+         <button className="btn btn-outiline-success" onClick={()=>handleFilter("category","men's clothing")}>MensClothing</button>
+        </li>
         </li>
         <li className="nav-item">
-          <Link className="nav-link" to={''}>
-            Jewellary
-          </Link>
+        <button className="btn btn-outiline-success" onClick={()=>handleFilter("category","jewelery")}>MensClothing</button>
         </li>
         <li className="nav-item">
-          <Link className="nav-link disabled" aria-disabled="true">
-            Mens Clothing
-          </Link>
+        <button className="btn btn-outiline-success" onClick={()=>handleFilter("category","women's clothing")}>WomensClothing</button>
         </li>
         <li className="nav-item">
-          <Link className="nav-link disabled" aria-disabled="true">
-            women'sMens Clothing
-          </Link>
+        <button className="btn btn-outiline-success" onClick={()=>handleFilter("category","electronics")}>electronics</button>
         </li>
       </ul>
+     {/* sorting */}
+    <div className="sorting-buttons">
+    <button className="btn btn-success" onClick={()=>handleFilter("sort","asc")}>LowtoHigh</button>
+    <button className="btn btn-danger" onClick={()=>handleFilter("sort","des")}>HightoLow</button>
     </div>
+
+    </div>
+
+    {/* displaying the Products */} 
+    <div className="container products">
+      <div className="row gy-2">
+        {
+          filterData.map((prod)=>(
+            <div className="col-sm-12 col-md-3 col-lg-3">
+             <Products  product={prod} key={prod.id}/>
+            </div>
+          ))
+        }
+      </div>
+    </div>
+
+    </>
   );
 };
 
--- lazyguy/src/pages/Products.jsx
+++ lazyguy/src/pages/Products.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 
-const Products = () => {
+const Products = ({product}) => {
+  const {title,price,description,category,image,rating:{rate,count}}=product
   return (
-    <div>
-      
+    <div class="card p-3" >
+    <img src={image} class="card-img-top" alt="..." style={{width:"100%", height:"150px"}}/>
+    <div class="card-body">
+      <h5 class="card-title">{title.slice(0,50)}</h5>
+      <p class="card-text">{description.slice(0,100)}...</p>
+      <p class="card-text ">Price: <span className='bg-success px-2'>{price}</span></p>
+      <a href="#" class="btn btn-primary">ADDTOCART</a>
     </div>
+  </div>
   )
 }
 
